623537577c2116db0db8e16dbbe3f5e3ce947c21
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-03-21 22:19:22 -0700

    style: add comment for wrapper function.

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -185,6 +185,7 @@ def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
+            """Validate and check permission from JWT."""
             token = get_token_auth_header()
             payload = verify_decode_jwt(token)
             check_permissions(permission, payload)
