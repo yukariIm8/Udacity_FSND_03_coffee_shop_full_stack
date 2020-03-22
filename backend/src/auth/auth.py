90f18a791e920ab717e625783c41d62f7d3eda7e
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-03-21 22:02:07 -0700

    refactor: remove exception_error_handler(error).

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -9,29 +9,33 @@ AUTH0_DOMAIN = 'dev-heuristic.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'coffeeshop'
 
-## AuthError Exception
+"""AuthError Exception"""
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
 '''
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
 
 
-## Auth Header
+"""Auth Header"""
 
 '''
 @TODO implement get_token_auth_header() method
     it should attempt to get the header from the request
-        it should raise an AuthError if no header is present
+    it should raise an AuthError if no header is present
     it should attempt to split bearer and the token
-        it should raise an AuthError if the header is malformed
+    it should raise an AuthError if the header is malformed
     return the token part of the header
 '''
+
+
 def get_token_auth_header():
-    """Obtain the Access Token from the Authorization Header."""
+    """Obtain the Authorization header from the request."""
     auth = request.headers.get('Authorization', None)
     if not auth:
         raise AuthError({
@@ -67,12 +71,17 @@ def get_token_auth_header():
         permission: string permission (i.e. 'post:drink')
         payload: decoded jwt payload
 
-    it should raise an AuthError if permissions are not included in the payload
-        !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
+    it should raise an AuthError if permissions are
+    not included in the payload
+    !!NOTE check your RBAC settings in Auth0
+    it should raise an AuthError if the requested permission
+    string is not in the payload permissions array
     return true otherwise
 '''
+
+
 def check_permissions(permission, payload):
+    """Check permission in JWT payload."""
     if 'permissions' not in payload:
         raise AuthError({
             'code': 'invalid_claims',
@@ -97,9 +106,13 @@ def check_permissions(permission, payload):
     it should validate the claims
     return the decoded payload
 
-    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
+    !!NOTE urlopen has a common certificate error described here:
+    https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
+
+
 def verify_decode_jwt(token):
+    """Verify and decode the JWT using the Auth0 secret."""
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
@@ -140,7 +153,8 @@ def verify_decode_jwt(token):
         except jwt.JWTClaimsError:
             raise AuthError({
                 'code': 'invalid_claims',
-                'description': 'Incorrect claims. Please, check the audience and issuer.'
+                'description': 'Incorrect claims.\
+                Please, check the audience and issuer.'
             }, 401)
         except Exception:
             raise AuthError({
@@ -159,9 +173,13 @@ def verify_decode_jwt(token):
 
     it should use the get_token_auth_header method to get the token
     it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
+    it should use the check_permissions method validate claims
+    and check the requested permission
+    return the decorator which passes the decoded payload
+    to the decorated method
 '''
+
+
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
